8a1deb63b9ab436017d75c3f644f4351145c1529
lherbut <lucjan.herbut@gmail.com>
2018-04-16 22:43:49 +0200

    Expense added with relationships

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -2,12 +2,14 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ExpensesCashAccountModule } from './cash-account/cash-account.module';
 import { ExpensesCategoryModule } from './category/category.module';
+import { ExpensesExpenseModule } from './expense/expense.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         ExpensesCashAccountModule,
         ExpensesCategoryModule,
+        ExpensesExpenseModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
